fa52857c9c50225516e0137c1a1f6ba696cc73d3
subu <subramanya4shenoy@gmail.com>
2021-12-15 02:09:36 +0530

    tailwid and introduction to spline

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,12 @@
+import React from 'react'
 
 export const App = () => {
-  return (<> empty project </>);
+  return (
+  <div className="w-full h-full"> 
+    tailwind is up
+    <div className='w-screen h-screen overflow-visible'>
+      <iframe src='https://my.spline.design/noisedisplacecopy-d225e306cbce9953001808f16cc1f798/' frameborder='0' width='100%' height='100%'></iframe>
+    </div>
+  </div>
+  );
 }
