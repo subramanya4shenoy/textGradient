181837d1db76750736ad39e1b3f350b770c1fb79
subu <subramanya4shenoy@gmail.com>
2021-12-16 22:04:25 +0530

    Routes

---
+++ client/src/Pages/Page404.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Page404() {
+    return (
+        <div>
+            looks Like URL is broken
+        </div>
+    )
+}
+
+export default Page404
---
+++ client/src/component/macro/CommonPageDecoration.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ThemeSwitch from '../micro/ThemSwitch'
+
+function CommonPageDecoration() {
+    return (
+        <div>
+          Test
+          <ThemeSwitch/>
+        </div>
+    )
+}
+
+export default CommonPageDecoration
