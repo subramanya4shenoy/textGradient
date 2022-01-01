76d2086b731ae09b36efd1d6aa399b1baea14140
subu <subramanya4shenoy@gmail.com>
2022-01-01 21:51:25 +0530

    code block

--- client/src/component/macro/CodeBox.js
+++ client/src/component/macro/CodeBox.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
-function CodeBox({css}) {
+function CodeBox({ css }) {
     return (
-        <div>
-           CodeBox {css}
+        <div className='w-auto mx-auto rounded-2xl h-auto bg-secondary p-6'>
+            <div className='mx-auto rounded-2xl  mx-auto w-full h-full border-0 bg-white p-4'>
+            <code className='text-primary text-xs'>
+            <pre>{css}</pre>
+            </code>
+            </div>
+            copy CSS
         </div>
     )
 }
