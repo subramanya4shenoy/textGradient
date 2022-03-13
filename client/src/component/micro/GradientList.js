0bc5d7e032907ae5783827a1d6cb0ee02c4f9f11
subu <subramanya4shenoy@gmail.com>
2022-03-13 22:24:01 +0530

    gradients in marquee

--- client/src/component/micro/GradientList.js
+++ client/src/component/micro/GradientList.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
 function GradientList({gradient}) {
+    
     return (
         <div className='flex'>
-            <div className='h-4 w-10 rounded-sm bg-accent'></div>
+            <div className='h-4 w-10 rounded-sm' 
+                style={{background: gradient.colors[0], 
+                        background: `linear-gradient(45deg, ${[...gradient.colors]} 100%)`}}></div>
             <div className='ml-2 mr-8 text-white ubuntu text-xs'> {gradient.name} </div>
         </div>
     )
