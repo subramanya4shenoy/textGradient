500fdc33eb40a5f05b89e48cb2d31f23894f58a7
subu <subramanya4shenoy@gmail.com>
2022-03-13 22:29:19 +0530

    theme texts

--- client/src/component/micro/GradientList.js
+++ client/src/component/micro/GradientList.js
@@ -7,7 +7,7 @@ function GradientList({gradient}) {
             <div className='h-4 w-10 rounded-sm' 
                 style={{background: gradient.colors[0], 
                         background: `linear-gradient(45deg, ${[...gradient.colors]} 100%)`}}></div>
-            <div className='ml-2 mr-8 text-white ubuntu text-xs'> {gradient.name} </div>
+            <div className='ml-2 mr-8 ubuntu text-xs dark:text-white light:text-secondary '> {gradient.name} </div>
         </div>
     )
 }
